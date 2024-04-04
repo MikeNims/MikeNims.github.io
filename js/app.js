364e0d8fba20c7609b97745a281a8bf182e5e351
MikeNims <michael.p.nims@gmail.com>
2024-04-04 01:29:55 -0400

    feat added styling for menu icon and navbar

--- js/app.js
+++ js/app.js
@@ -127,7 +127,13 @@
 // Linkedin.append(LinkedinClass);
 
 /*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
 /*==================== scroll sections active link ====================*/
 
 /*==================== sticky navbar ====================*/
@@ -136,7 +142,8 @@ header.classList.toggle('sticky', window.scrollY > 100);
 
 
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
 
