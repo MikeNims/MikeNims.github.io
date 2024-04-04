371e4302fe8e33beffb847616e922c2b333c1af7
MikeNims <michael.p.nims@gmail.com>
2024-04-04 01:21:02 -0400

    feat: added styling for various media screen sizes, styling header

--- js/app.js
+++ js/app.js
@@ -1,147 +1,143 @@
-//——— app.js
-// doc setup
-console.log('---app.js---')
-const body = document.body;
-console.log(body)
-const bodyChildren = [...body.children]
-console.log(bodyChildren)
+// //——— app.js
+// // doc setup
+// console.log('---app.js---')
+// const body = document.body;
+// console.log(body)
+// const bodyChildren = [...body.children]
+// console.log(bodyChildren)
 
-//header
-const section=document.createElement('header');
-section.setAttribute('class','header');
-console.log(section)
-body.appendChild(section)
+// //header
+// const section=document.createElement('header');
+// section.setAttribute('class','header');
+// console.log(section)
+// body.appendChild(section)
 
-const anchor = document.createElement('a');
-anchor.setAttribute('class', 'logo')
-anchor.textContent = "portfolio";
-section.appendChild(anchor);
-console.log(anchor)
+// const anchor = document.createElement('a');
+// anchor.setAttribute('class', 'logo')
+// anchor.textContent = "portfolio";
+// section.appendChild(anchor);
+// console.log(anchor)
 
-const i = document.createElement('i');
-i.setAttribute('class','bx bx-menu')
-i.setAttribute('id','menu-icons')
-console.log(i)
+// const i = document.createElement('i');
+// i.setAttribute('class','bx bx-menu')
+// i.setAttribute('id','menu-icons')
+// console.log(i)
 
-section.appendChild(i)
-
-
-
-//navbar
-const navbar = document.createElement('nav');
-navbar.setAttribute('class','navbar')
-section.appendChild(navbar)
-console.log(navbar)
+// section.appendChild(i)
 
 
-const aboutAnchor = document.createElement('a');
-aboutAnchor.textContent = "about";
-navbar.appendChild(aboutAnchor);
-console.log(aboutAnchor)
 
-const svcAnchor = document.createElement('a');
-svcAnchor.textContent = "Services";
-navbar.appendChild(svcAnchor)
-console.log(svcAnchor)
-
-const portAnchor = document.createElement('a');
-portAnchor.textContent = "Portfolio";
-navbar.appendChild(portAnchor);
-console.log(portAnchor);
-
-const contactAnchor = document.createElement('a');
-contactAnchor.textContent = "Contact";
-navbar.appendChild(contactAnchor);
-console.log(contactAnchor)
+// //navbar
+// const navbar = document.createElement('nav');
+// navbar.setAttribute('class','navbar')
+// section.appendChild(navbar)
+// console.log(navbar)
 
 
-//home
-const home = document.createElement('section');
-home.setAttribute('class','home');
-home.setAttribute('id','home');
-body.appendChild(home);
+// const aboutAnchor = document.createElement('a');
+// aboutAnchor.textContent = "about";
+// navbar.appendChild(aboutAnchor);
+// console.log(aboutAnchor)
 
-//home content
-const homecontent = document.createElement('Div');
-homecontent.setAttribute('class','home-content');
-home.appendChild(homecontent);
+// const svcAnchor = document.createElement('a');
+// svcAnchor.textContent = "Services";
+// navbar.appendChild(svcAnchor)
+// console.log(svcAnchor)
 
-const h3 = document.createElement('h3');
-h3.textContent = "Hello it's Me";
-homecontent.appendChild(h3)
+// const portAnchor = document.createElement('a');
+// portAnchor.textContent = "Portfolio";
+// navbar.appendChild(portAnchor);
+// console.log(portAnchor);
 
-const h1 = document.createElement('h1');
-h1.textContent = "Mike Nims";
-homecontent.appendChild(h1);
-
-const title = document.createElement('h3')
-title.textContent = "I'm a frontend engineer"
-homecontent.appendChild(title);
-
-const para = document.createElement("p");
-para.textContent = ` Lorem, ipsum dolor sit amet consectetur
-adipisicing elit. Eaque nihil id at soluta, 
-illum sequi. Maxime, rem`
-homecontent.appendChild(para)
-
-//social-media
-const socialMedia = document.createElement('div');
-socialMedia.setAttribute('class','social-media');
-body.appendChild(socialMedia);
-
-//fb
-const facebook = document.createElement("a")
-facebook.href = "#";
-socialMedia.append(facebook);
-
-const facebookClass = document.createElement("i");
-facebookClass.className = "bx bx1-facebook";
-facebook.append(facebookClass);
+// const contactAnchor = document.createElement('a');
+// contactAnchor.textContent = "Contact";
+// navbar.appendChild(contactAnchor);
+// console.log(contactAnchor)
 
 
-//twitter
-const twitter = document.createElement("a")
-twitter.href = "#";
-socialMedia.append(twitter);
+// //home
+// const home = document.createElement('section');
+// home.setAttribute('class','home');
+// home.setAttribute('id','home');
+// body.appendChild(home);
 
-const twitterClass = document.createElement("i");
-twitterClass.className = "bx bx1-twitter";
-twitter.append(twitterClass);
+// //home content
+// const homecontent = document.createElement('Div');
+// homecontent.setAttribute('class','home-content');
+// home.appendChild(homecontent);
 
-//ig
-const instagram = document.createElement("a")
-instagram.href = "#";
-socialMedia.append(instagram);
+// const h3 = document.createElement('h3');
+// h3.textContent = "Hello it's Me";
+// homecontent.appendChild(h3)
 
-const instagramClass = document.createElement("i");
-instagramClass.className = "bx bx1-instagram";
-instagram.append(instagramClass);
+// const h1 = document.createElement('h1');
+// h1.textContent = "Mike Nims";
+// homecontent.appendChild(h1);
 
-//Linkedin
-const Linkedin = document.createElement("a")
-Linkedin.href = "#";
-socialMedia.append(Linkedin);
+// const title = document.createElement('h3')
+// title.textContent = "I'm a frontend engineer"
+// homecontent.appendChild(title);
 
-const LinkedinClass = document.createElement("i");
-LinkedinClass.className = "bx bx1-Linkedin";
-Linkedin.append(LinkedinClass);
+// const para = document.createElement("p");
+// para.textContent = ` Lorem, ipsum dolor sit amet consectetur
+// adipisicing elit. Eaque nihil id at soluta, 
+// illum sequi. Maxime, rem`
+// homecontent.appendChild(para)
+
+// //social-media
+// const socialMedia = document.createElement('div');
+// socialMedia.setAttribute('class','social-media');
+// body.appendChild(socialMedia);
+
+// //fb
+// const facebook = document.createElement("a")
+// facebook.href = "#";
+// socialMedia.append(facebook);
+
+// const facebookClass = document.createElement("i");
+// facebookClass.className = "bx bx1-facebook";
+// facebook.append(facebookClass);
+
+
+// //twitter
+// const twitter = document.createElement("a")
+// twitter.href = "#";
+// socialMedia.append(twitter);
+
+// const twitterClass = document.createElement("i");
+// twitterClass.className = "bx bx1-twitter";
+// twitter.append(twitterClass);
+
+// //ig
+// const instagram = document.createElement("a")
+// instagram.href = "#";
+// socialMedia.append(instagram);
+
+// const instagramClass = document.createElement("i");
+// instagramClass.className = "bx bx1-instagram";
+// instagram.append(instagramClass);
+
+// //Linkedin
+// const Linkedin = document.createElement("a")
+// Linkedin.href = "#";
+// socialMedia.append(Linkedin);
+
+// const LinkedinClass = document.createElement("i");
+// LinkedinClass.className = "bx bx1-Linkedin";
+// Linkedin.append(LinkedinClass);
 
 /*==================== toggle icon navbar ====================*/
 
 /*==================== scroll sections active link ====================*/
 
 /*==================== sticky navbar ====================*/
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
 
 
-/*==================== toggle icon navbar ====================*/
-/*==================== scroll sections active link ====================*/
-
-/*==================== sticky navbar ====================*/
-/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-
-/*==================== scroll reveal ====================*/
-/*==================== typed js ====================*/
