3afa42706b2438097ce8dc35ca63a4d31756ef44
MikeNims <michael.p.nims@gmail.com>
2024-04-06 15:38:38 -0400

    added styling for 450px and 365px

--- js/app.js
+++ js/app.js
@@ -149,13 +149,12 @@ ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200,
+    delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .contact form', {origin: 'bottom'});
-ScrollReveal().reveal('.come-content h1', '.about-img', {origin: 'left'})
-
+ScrollReveal().reveal(".home-content, .heading", { origin: "top"});
+ScrollReveal().reveal(".home-img, .services-container, .contact form", { origin: "bottom"});
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 /*==================== typed js ====================*/
 
 
