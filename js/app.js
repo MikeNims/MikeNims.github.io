3f5a50a652d7390f6114eeff3fffe1951bea6939
MikeNims <michael.p.nims@gmail.com>
2024-04-06 17:29:24 -0400

    feat: centering elements in body

--- js/app.js
+++ js/app.js
@@ -152,9 +152,17 @@ ScrollReveal({
     delay: 200
 });
 
-ScrollReveal().reveal(".home-content, .heading", { origin: "top"});
-ScrollReveal().reveal(".home-img, .services-container, .contact form", { origin: "bottom"});
-ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Engineer', 'Backend Engineer', 'FullStack Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000, 
+    loop: true
+});
 
 
