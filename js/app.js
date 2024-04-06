d0fb8d32f9c5c644a6edbc507e4abecbb895eb38
MikeNims <michael.p.nims@gmail.com>
2024-04-06 15:06:39 -0400

    feat:added style for 615 px  added scroll reveal

--- js/app.js
+++ js/app.js
@@ -145,6 +145,17 @@ header.classList.toggle('sticky', window.scrollY > 100);
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 /*==================== scroll reveal ====================*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.come-content h1', '.about-img', {origin: 'left'})
+
 /*==================== typed js ====================*/
 
 
